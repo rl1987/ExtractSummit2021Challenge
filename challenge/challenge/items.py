e176d4243f98805ebd3de7d67cdd2c619d8cdf54
rl1987 <rl1987@users.noreply.github.com>
2021-09-30 17:40:46 +0300

    Make some fields for item

--- challenge/challenge/items.py
+++ challenge/challenge/items.py
@@ -8,5 +8,8 @@ import scrapy
 
 class ChallengeItem(scrapy.Item):
     # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+    item_id = scrapy.Field()
+    name = scrapy.Field()
+    image_id = scrapy.Field()
+    flavor = scrapy.Field()
+    
