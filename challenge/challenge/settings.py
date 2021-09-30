cac4e357172ce50955b4878d82ee9360ab847686
rl1987 <rl1987@users.noreply.github.com>
2021-09-30 17:36:44 +0300

    Start working on the spider

--- challenge/challenge/settings.py
+++ challenge/challenge/settings.py
@@ -17,7 +17,7 @@ NEWSPIDER_MODULE = 'challenge.spiders'
 #USER_AGENT = 'challenge (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
--- challenge/challenge/spiders/contest.py
+++ challenge/challenge/spiders/contest.py
@@ -1,10 +1,24 @@
 import scrapy
 
+from urllib.parse import urljoin, urlparse
 
 class ContestSpider(scrapy.Spider):
     name = 'contest'
     allowed_domains = ['contest-646508-5umjfyjn4a-ue.a.run.app']
-    start_urls = ['http://contest-646508-5umjfyjn4a-ue.a.run.app/']
+    start_urls = ['https://contest-646508-5umjfyjn4a-ue.a.run.app/listing?page=-1']
 
-    def parse(self, response):
+    def start_requests(self):
+        yield scrapy.Request(self.start_urls[0], callback=self.parse_product_list)
+
+    def parse_product_list(self, response):
+        for product_link in response.xpath('//a[contains(@href, "/listing/i/")]/@href').getall():
+            product_url = urljoin(response.url, product_link)
+            yield scrapy.Request(product_url, callback=self.parse_product_page)
+
+        next_page_link = response.xpath('//a[contains(@href, "page=")]/@href').get()
+        if next_page_link is not None:
+            next_page_url = urljoin(response.url, next_page_link)
+            yield scrapy.Request(next_page_url, callback=self.parse_product_list)
+
+    def parse_product_page(self, response):
         pass
